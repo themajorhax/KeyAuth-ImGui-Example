98bdab28ba6d5063df1de5d83c9daec915935f1e
MajorHax <79766615+themajorhax@users.noreply.github.com>
2022-01-28 19:44:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# KeyAuth-Imgui-key-Login
+This is a imgui login that runs with keyauth with only uses the key and has tabs for you to paste your stuff in c++
+
+
+KeyAuth CPP IMGUI Example
+
+Video on how to add KeyAuth to your own application https://youtu.be/GB4XW_TsHqA
+MIGHT NOT HELP NOT MY VIDEO
+
+Video to use Web Loader (control loader from customer panel) https://youtu.be/9-qgmsUUCK4
+MIGHT NOT HELP NOT MY VIDEO
+
+
+If you need help join the discord
+
+https://discord.gg/bR5zdvXaVU
+
+lucent.vip#7537
+
+Just a base dosent look pretty
+
+
+**Run**
+**In**
+**Release 64x**
+
+
+
+How to do it:
+
+To fix this error
+Property page > C/C++ > Optimization > Whole program Optimization > No
+
+open the .sln file
+
+change to Release mode and x64
+
+Change your Secret,Owner id etc. (found on KeyAuth)
+
+build it!
+
+
+Project settings
+https://media.discordapp.net/attachments/927012865712484372/932890231311564800/Untitled.png?width=720&height=497
+https://media.discordapp.net/attachments/927012865712484372/932890448916271114/Untitled.png?width=720&height=499
+https://media.discordapp.net/attachments/927012865712484372/932890631074889748/da.png?width=720&height=498
+https://media.discordapp.net/attachments/927012865712484372/932890631284588584/lol.png?width=720&height=497
---
+++ main.cpp
@@ -0,0 +1,343 @@
+#include "Main.h"
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <winbase.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <string>
+#include "xorstr.hpp"
+#include "auth.hpp"
+#include <filesystem>
+#include <TlHelp32.h>
+
+#pragma region Bool & Int & NameSpace
+
+using namespace KeyAuth;
+using namespace std;
+
+static bool showing = true;
+
+static int currentColumn = 0;
+
+#pragma comment(lib,"Wininet.lib")
+#pragma comment(lib, "winmm.lib")
+
+#define _WIN32_WINNT 0x0500
+
+#pragma endregion
+
+#pragma region HideConsole
+void xxx()
+{
+    HWND Stealth;
+    AllocConsole();
+    Stealth = FindWindowA("ConsoleWindowClass", NULL);
+    ShowWindow(Stealth, 0);
+}
+#pragma endregion
+
+#pragma region WinMain
+
+int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    std::string name = XorStr("tezzz"); // Application Name
+    std::string ownerid = XorStr("NcHVSpWL1g"); // User OwnerID
+    std::string secret = XorStr("28a057b8f892d1d23c0be574cd35b2c71073bea9ed2b8deb4a8ef9d2d2b01815"); // Application Secret
+    std::string version = XorStr("1.0"); // Application Version
+
+    api KeyAuthApp(name, ownerid, secret, version);
+
+    xxx();
+    KeyAuthApp.init();
+    if (!KeyAuthApp.data.success)
+    {
+        std::cout << "\n Status: " + KeyAuthApp.data.message;
+        Sleep(500);
+        exit(0);
+    }
+    KeyAuthApp.init(); // Initilize Auth  - Dont Touch It !
+
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, LOADER_BRAND, NULL };
+    RegisterClassEx(&wc);
+    main_hwnd = CreateWindow(wc.lpszClassName, LOADER_BRAND, WS_POPUP, 0, 0, 5, 5, NULL, NULL, wc.hInstance, NULL);
+
+    if (!CreateDeviceD3D(main_hwnd)) {
+        CleanupDeviceD3D();
+        UnregisterClass(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
+
+    ShowWindow(main_hwnd, SW_HIDE);
+    UpdateWindow(main_hwnd);
+
+    ImGui::CreateContext();
+
+    #pragma region Theme
+
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.IniFilename = nullptr;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGui::StyleColorsDark();
+
+    // Add Custom Font
+    io.Fonts->AddFontDefault();
+
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        void Theme();
+        {
+
+            ImGui::GetStyle().FrameRounding = 4.0f;
+            ImGui::GetStyle().GrabRounding = 4.0f;
+
+            ImVec4* colors = ImGui::GetStyle().Colors;
+            colors[ImGuiCol_Text] = ImVec4(0.95f, 0.96f, 0.98f, 1.00f);
+            colors[ImGuiCol_TextDisabled] = ImVec4(0.36f, 0.42f, 0.47f, 1.00f);
+            colors[ImGuiCol_WindowBg] = ImColor(0, 0, 0, 255);
+            colors[ImGuiCol_ChildBg] = ImVec4(0.15f, 0.18f, 0.22f, 1.00f);
+            colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
+            colors[ImGuiCol_Border] = ImColor(0, 0, 0);
+            colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+            colors[ImGuiCol_FrameBg] = ImVec4(0.20f, 0.25f, 0.29f, 1.00f);
+            colors[ImGuiCol_FrameBgHovered] = ImVec4(0.12f, 0.20f, 0.28f, 1.00f);
+            colors[ImGuiCol_FrameBgActive] = ImVec4(0.09f, 0.12f, 0.14f, 1.00f);
+            colors[ImGuiCol_TitleBg] = ImColor(0, 0, 0, 255);
+            colors[ImGuiCol_TitleBgActive] = ImVec4(0.08f, 0.10f, 0.12f, 1.00f);
+            colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+            colors[ImGuiCol_MenuBarBg] = ImVec4(0.15f, 0.18f, 0.22f, 1.00f);
+            colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.39f);
+            colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.20f, 0.25f, 0.29f, 1.00f);
+            colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.18f, 0.22f, 0.25f, 1.00f);
+            colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.09f, 0.21f, 0.31f, 1.00f);
+            colors[ImGuiCol_CheckMark] = ImVec4(0.28f, 0.56f, 1.00f, 1.00f);
+            colors[ImGuiCol_SliderGrab] = ImVec4(0.28f, 0.56f, 1.00f, 1.00f);
+            colors[ImGuiCol_SliderGrabActive] = ImVec4(0.37f, 0.61f, 1.00f, 1.00f);
+            colors[ImGuiCol_Button] = ImVec4(0.20f, 0.25f, 0.29f, 1.00f);
+            colors[ImGuiCol_ButtonHovered] = ImVec4(0.28f, 0.56f, 1.00f, 1.00f);
+            colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
+            colors[ImGuiCol_Header] = ImColor(0, 0, 0);
+            colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+            colors[ImGuiCol_HeaderActive] = ImColor(0, 0, 0);
+            colors[ImGuiCol_Separator] = ImVec4(0.20f, 0.25f, 0.29f, 1.00f);
+            colors[ImGuiCol_SeparatorHovered] = ImVec4(0.10f, 0.40f, 0.75f, 0.78f);
+            colors[ImGuiCol_SeparatorActive] = ImVec4(0.10f, 0.40f, 0.75f, 1.00f);
+            colors[ImGuiCol_ResizeGrip] = ImVec4(0.26f, 0.59f, 0.98f, 0.25f);
+            colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+            colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+            colors[ImGuiCol_Tab] = ImVec4(0.11f, 0.15f, 0.17f, 1.00f);
+            colors[ImGuiCol_TabHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+            colors[ImGuiCol_TabActive] = ImVec4(0.20f, 0.25f, 0.29f, 1.00f);
+            colors[ImGuiCol_TabUnfocused] = ImVec4(0.11f, 0.15f, 0.17f, 1.00f);
+            colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.11f, 0.15f, 0.17f, 1.00f);
+            colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+            colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+            colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+            colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+            colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+            colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+            colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+            colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+            colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+            colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+        }
+    }
+
+#pragma endregion
+
+    ImGui_ImplWin32_Init(main_hwnd);
+    ImGui_ImplDX9_Init(g_pd3dDevice);
+
+    DWORD window_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+
+    RECT screen_rect;
+    GetWindowRect(GetDesktopWindow(), &screen_rect);
+    auto x = float(screen_rect.right - WINDOW_WIDTH) / 2.f;
+    auto y = float(screen_rect.bottom - WINDOW_HEIGHT) / 2.f;
+
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            continue;
+        }
+
+
+        ImGui_ImplDX9_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+        {
+            ImGui::SetNextWindowPos(ImVec2(x, y), ImGuiCond_Once);
+            ImGui::SetNextWindowSize(ImVec2(WINDOW_WIDTH, WINDOW_HEIGHT));
+            ImGui::SetNextWindowBgAlpha(1.0f);
+
+            ImGui::Begin(LOADER_BRAND, &loader_active, window_flags);
+            {
+
+                ImGuiIO& IO = ImGui::GetIO(); (void)IO;
+                DefaultFont = IO.Fonts->AddFontFromFileTTF(".\\SegoeUI.ttf", 20.0f, NULL, IO.Fonts->GetGlyphRangesCyrillic());
+
+                if (currentColumn == 0) {
+                    
+                    ImGui::SetCursorPos(ImVec2(8, 28.5));
+
+                    ImGui::TextColored(ImVec4(1.0f, 0.7f, 0.0f, 1.0f), "KeyAuth ImGui Example - By MajorHax");
+                    ImGui::Text("");
+                    //ImGui::SameLine(50);
+
+                    ImGui::SetNextWindowPos(ImVec2(305, 300));
+                    ImGui::SetNextWindowSize(ImVec2(400, 400));
+
+
+                    ImGui::InputText("Username", username, sizeof(username), 0, 0, 0);
+                    ImGui::Separator();
+
+                    ImGui::InputText("Password", password, sizeof(password), ImGuiInputTextFlags_Password, 0, 0);
+                    ImGui::Separator();
+
+                    ImGui::InputText("License", key, sizeof(key), 0, 0, 0);
+                    ImGui::Separator();
+
+
+
+                    ImGui::PushItemWidth(200);
+                    ImGui::PopItemWidth();
+
+                    ImGui::Text("");
+
+                    if (ImGui::Button("Login Now")) {
+                        KeyAuthApp.login(username, password);
+                        if (KeyAuthApp.data.success) {
+                            MessageBoxA(0, KeyAuthApp.data.message.c_str(), "Successful Login", 0);
+                            KeyAuthApp.log("Logged In");
+                            currentColumn = 1;
+                        }
+                        else if (!KeyAuthApp.data.success) {
+                            MessageBoxA(0, KeyAuthApp.data.message.c_str(), "Failed Login", 0);
+                        }
+                    }
+
+                    ImGui::Separator();
+                    ImGui::Spacing();
+                    //ImGui::Text("") //Add space between Login and Register Buttons
+
+                    if (ImGui::Button("Register Now")) {
+                        KeyAuthApp.regstr(username, password, key);
+                        if (KeyAuthApp.data.success) {
+                            MessageBoxA(0, KeyAuthApp.data.message.c_str(), "Successful Registration", 0);
+                        }
+                        else if (!KeyAuthApp.data.success) {
+                            MessageBoxA(0, KeyAuthApp.data.message.c_str(), "Failed To Register", 0);
+                        }
+                    }
+                }
+            
+
+                if (currentColumn == 1)
+                {
+                    ImGui::SetCursorPos(ImVec2(8, 28.5));
+                    ImGui::PushItemWidth(200);
+                    ImGui::PopItemWidth();
+
+                    ImGui::Text(" ");
+                    ImGui::TextColored(ImColor(250, 251, 252), "KeyAuth ImGui Example By MajorHax");
+
+                    ImGui::Text("");
+                    ImGui::Text("");
+
+                 //   ImGui::Text(" Your code goes here ");
+
+                    ImGui::Text("");
+
+                    if (ImGui::Button("Exit"))
+                    {
+                        exit(3);
+                        return 3;
+                    }
+                }
+            }
+            ImGui::End();
+        }
+        ImGui::EndFrame();
+
+        g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0, 1.0f, 0);
+        if (g_pd3dDevice->BeginScene() >= 0)
+        {
+            ImGui::Render();
+            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+            g_pd3dDevice->EndScene();
+        }
+
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+        }
+
+        HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+
+        if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET) {
+            ResetDevice();
+        }
+        if (!loader_active) {
+            msg.message = WM_QUIT;
+        }
+    }
+
+    ImGui_ImplDX9_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceD3D();
+    DestroyWindow(main_hwnd);
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+    return 0;
+}
+
+
+#pragma endregion
+
+#pragma region SomeShit Dont Touch
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            g_d3dpp.BackBufferWidth = LOWORD(lParam);
+            g_d3dpp.BackBufferHeight = HIWORD(lParam);
+            ResetDevice();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU)
+            return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+
+
+}
+
+#pragma endregion
